dba12a65fd466e7a35893ad382cd24219f328f79
tiaeastwood <teastwood88@gmail.com>
2020-07-22 14:25:08 +0100

    implement suggestions made my lighthouse audit

--- script.js
+++ script.js
@@ -69,3 +69,7 @@ function topFunction() {
 	document.body.scrollTop = 0; // For Safari
 	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+// Display date
+const year = new Date().getFullYear();
+document.getElementById("datetime").innerHTML = year;
